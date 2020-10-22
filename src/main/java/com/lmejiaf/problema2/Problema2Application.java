51a77a8f9e9d6ac788fb443f7a6f576c565aa665
lmejiaf <lmejiaf@uninorte.edu.co>
2020-10-21 22:07:08 -0500

    initial commit

---
+++ src/main/java/com/lmejiaf/problema2/Problema2Application.java
@@ -0,0 +1,13 @@
+package com.lmejiaf.problema2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Problema2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Problema2Application.class, args);
+	}
+
+}
